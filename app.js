7d22822a4f820486064ad1d5ecaaa30c725127f2
LazyLock <a01058121097@gmail.com>
2022-11-12 16:50:15 +0900

    기본적인 구조 작성
    localhost:1708로 접근하면 views/main.html 페이지 렌더링
    
    * git clone 이후 터미널에서 npm i 실행해야 코드 돌아가는 것 참고

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const path = require("path");
+const dotenv = require("dotenv");
+
+const mainRouter = require("./routes")
+
+dotenv.config();
+
+const app = express();
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', mainRouter)
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen("1708", () => {
+  console.log("1708번 포트에 연결을 성공하였습니다.");
+});
