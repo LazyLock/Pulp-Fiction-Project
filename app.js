d8410edd11fcb83cdb9aad1add104dd6e0f318f0
LazyLock <a01058121097@gmail.com>
2022-11-17 17:31:32 +0900

    외부 사용자 입장에서 test
    - css 파일 옮기기
    - 라우터 경로 지정

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 
 const mainRouter = require("./routes")
+const externalRouter = require("./routes/external_test")
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', mainRouter)
+app.use('/test', externalRouter)
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
