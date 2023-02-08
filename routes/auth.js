02bb4e6fcba6ebcebd452cfca97e3d476497db97
lazylock <lazylock99@gmail.com>
2023-02-08 14:58:15 +0900

    로그인, 회원가입 기능 구현 마무리

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const models = require("../models");
 const bcrypt = require("bcrypt");
+const popup = require("node-popup");
 
 const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
-	const hashPassword = await bcrypt.hash(req.body.password, 11);
+  const hashPassword = await bcrypt.hash(req.body.password, 11);
   try {
     let result = await models.User.create({
       userID: req.body.id,
@@ -25,20 +26,24 @@ router.post("/login", async (req, res, next) => {
   let id = req.body.id;
   let pw = req.body.password;
 
-  const result = await models.User.findOne({
-    where: { userID: id },
-  });
+  try {
+    const result = await models.User.findOne({
+      where: { userID: id },
+    });
+    const dbPassword = result.password;
+    bcrypt.compare(pw, dbPassword, (err, same) => {
+      if (same) {
+        res.render("guidePage");
+      } else {
+        console.log("error!");
+        res.render("loginAlert", { error: "비밀번호 오류. 다시 입력해주세요" });
+      }
+    });
+  } catch(err) {
+    res.render("loginAlert", {error: '존재하지 않는 아이디입니다. 회원가입을 먼저 진행해주세요.'});
+  }
 
-  let dbPassword = result.password;
 
-	bcrypt.compare(pw, dbPassword, (err, same) => {
-		if (same) {
-			res.render("guidePage");
-		} else {
-			console.log("error!")
-			re
-		}
-	})
 });
 
 module.exports = router;
